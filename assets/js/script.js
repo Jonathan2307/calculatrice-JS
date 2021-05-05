6b237de65bd7d045dd415876be7480b55f4e8197
Jonathan2307 <gratigny.j@me.com>
2021-05-05 23:54:53 +0200

    add reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,33 @@
-const 
+const calcbutton = document.querySelectorAll('input[data-button="calcbutton"]');
+
+const operatebutton = document.querySelectorAll('input[data-button="operatebutton"]');
+
+const resultbutton = document.querySelectorAll('input[data-button="resultbutton"]');
+
+const resetbutton = document.querySelectorAll('input[data-reset="CE"]');
+console.log('reset selected');
+
+
+
+calcbutton.forEach(element => {
+    element.addEventListener('click', function () {
+        numberOne.value += this.dataset.number
+
+    })
+});
+
+operatebutton.forEach(element => {
+    element.addEventListener('click', function () {
+
+        operator.value = this.dataset.operator;
+
+    })
+});
+
+resetbutton.forEach(element => {
+    element.addEventListener('click', function () {
+        numberOne.value = ''
+        numberTwo.value = ''
+        operator.value = ''
+    })
+});
